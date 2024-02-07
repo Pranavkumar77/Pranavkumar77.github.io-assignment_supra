e84f44681923bd39ce943e6ebf162a7761ee8cfd
Pranavkumar Suresh Patil <60297786+Pranavkumar77@users.noreply.github.com>
2024-02-07 13:52:50 +0530

    Updates

---
+++ assets/PhotoItem-0s27H_-A.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-f2AVNhcC.js";const l=({photo:t})=>s.jsxs("li",{className:"photo-item",children:[s.jsx("img",{className:"thumbnail",src:t.thumbnailUrl,alt:t.title}),s.jsx("p",{className:"title",children:t.title})]});export{l as default};
